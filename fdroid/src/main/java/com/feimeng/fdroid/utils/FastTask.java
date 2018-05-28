73b150dc6d0060dfe15160eeb6d8d3eb4455e974
feimeng <feimenggo@163.com>
2018-05-28 10:21:54 +0800

    完善FastTask的结果回调方法

--- fdroid/src/main/java/com/feimeng/fdroid/utils/FastTask.java
+++ fdroid/src/main/java/com/feimeng/fdroid/utils/FastTask.java
@@ -2,6 +2,7 @@ package com.feimeng.fdroid.utils;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
@@ -11,8 +12,8 @@ import rx.schedulers.Schedulers;
  * Created by feimeng on 2017/6/10.
  */
 public abstract class FastTask<T> {
-    public void runCalc() {
-        Observable.create(new Observable.OnSubscribe<T>() {
+    public Subscription runCalc() {
+        return Observable.create(new Observable.OnSubscribe<T>() {
             @Override
             public void call(Subscriber<? super T> subscriber) {
                 subscriber.onNext(task());
@@ -21,8 +22,8 @@ public abstract class FastTask<T> {
         }).subscribeOn(Schedulers.computation()).subscribe();
     }
 
-    public void runCalc(Action1<T> action1) {
-        Observable.create(new Observable.OnSubscribe<T>() {
+    public Subscription runCalc(Action1<T> action1) {
+        return Observable.create(new Observable.OnSubscribe<T>() {
             @Override
             public void call(Subscriber<? super T> subscriber) {
                 subscriber.onNext(task());
@@ -31,8 +32,8 @@ public abstract class FastTask<T> {
         }).subscribeOn(Schedulers.computation()).observeOn(AndroidSchedulers.mainThread()).subscribe(action1);
     }
 
-    public void runCalc(Subscriber<T> subscriber) {
-        Observable.create(new Observable.OnSubscribe<T>() {
+    public Subscription runCalc(Subscriber<T> subscriber) {
+        return Observable.create(new Observable.OnSubscribe<T>() {
             @Override
             public void call(Subscriber<? super T> subscriber) {
                 subscriber.onNext(task());
@@ -41,8 +42,8 @@ public abstract class FastTask<T> {
         }).subscribeOn(Schedulers.computation()).observeOn(AndroidSchedulers.mainThread()).subscribe(subscriber);
     }
 
-    public void runIO() {
-        Observable.create(new Observable.OnSubscribe<T>() {
+    public Subscription runIO() {
+        return Observable.create(new Observable.OnSubscribe<T>() {
             @Override
             public void call(Subscriber<? super T> subscriber) {
                 subscriber.onNext(task());
@@ -51,8 +52,8 @@ public abstract class FastTask<T> {
         }).subscribeOn(Schedulers.io()).subscribe();
     }
 
-    public void runIO(Action1<T> action1) {
-        Observable.create(new Observable.OnSubscribe<T>() {
+    public Subscription runIO(Action1<T> action1) {
+        return Observable.create(new Observable.OnSubscribe<T>() {
             @Override
             public void call(Subscriber<? super T> subscriber) {
                 subscriber.onNext(task());
@@ -61,8 +62,8 @@ public abstract class FastTask<T> {
         }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(action1);
     }
 
-    public void runIO(Subscriber<T> subscriber) {
-        Observable.create(new Observable.OnSubscribe<T>() {
+    public Subscription runIO(Subscriber<T> subscriber) {
+        return Observable.create(new Observable.OnSubscribe<T>() {
             @Override
             public void call(Subscriber<? super T> subscriber) {
                 subscriber.onNext(task());
@@ -74,13 +75,49 @@ public abstract class FastTask<T> {
     public abstract T task();
 
     public static abstract class Result<R> extends Subscriber<R> {
-        @Override
-        public void onCompleted() {
 
+        @Override
+        public void onStart() {
+            start();
+        }
+
+        @Override
+        public void onNext(R r) {
+            success(r);
         }
 
         @Override
         public void onError(Throwable e) {
+            fail(e);
+            stop();
+        }
+
+        @Override
+        public void onCompleted() {
+            stop();
+        }
+
+        /**
+         * 任务开始
+         */
+        void start() {
+
+        }
+
+        /**
+         * 任务执行成功
+         */
+        abstract void success(R data);
+
+        /**
+         * 任务执行失败
+         */
+        abstract void fail(Throwable throwable);
+
+        /**
+         * 任务结束
+         */
+        void stop() {
 
         }
     }
