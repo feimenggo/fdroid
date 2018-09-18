5452941632804f45ada71a6db02478e74cb457eb
feimeng <feimenggo@163.com>
2018-09-18 11:20:01 +0800

    完善FDActivity LoadingDialog功能

--- fdroid/src/main/java/com/feimeng/fdroid/mvp/base/FDPresenter.java
+++ fdroid/src/main/java/com/feimeng/fdroid/mvp/base/FDPresenter.java
@@ -1,7 +1,9 @@
 package com.feimeng.fdroid.mvp.base;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresPermission;
 
 import com.feimeng.fdroid.base.FDActivity;
 import com.feimeng.fdroid.base.FDFragment;
@@ -96,11 +98,15 @@ public abstract class FDPresenter<V extends FDView> {
     }
 
     public void showDialog(String message) {
+        showDialog(message, true);
+    }
+
+    public void showDialog(String message, boolean cancelable) {
         if (mView == null) return;
         if (mView instanceof FDActivity)
-            ((FDActivity) mView).showLoadingDialog(message);
+            ((FDActivity) mView).showLoadingDialog(message, cancelable);
         else if (mView instanceof FDFragment)
-            ((FDFragment) mView).showLoadingDialog(message);
+            ((FDFragment) mView).showLoadingDialog(message, cancelable);
     }
 
     /**
@@ -154,12 +160,15 @@ public abstract class FDPresenter<V extends FDView> {
         void withoutNetwork(Object data);
     }
 
+    @RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
     public <T> Observable<T> withNet(Observable<T> task) {
         return withNet(task, false);
     }
 
+    @RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
     public <T> Observable<T> withNet(Observable<T> task, final Object data) {
         final Observable<T> checkNet = Observable.create(new ObservableOnSubscribe<T>() {
+            @SuppressLint("MissingPermission")
             @Override
             public void subscribe(ObservableEmitter<T> emitter) {
                 if (!NetworkUtil.isConnectingToInternet(getContext())) {
@@ -175,12 +184,15 @@ public abstract class FDPresenter<V extends FDView> {
         return Observable.concat(checkNet, task).firstElement().toObservable();
     }
 
+    @RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
     public <T> Observable<T> withNet(Observable<T> task, final OnWithoutNetwork network) {
         return withNet(task, network, null);
     }
 
+    @RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
     public <T> Observable<T> withNet(Observable<T> task, final OnWithoutNetwork network, final Object data) {
         final Observable<T> checkNet = Observable.create(new ObservableOnSubscribe<T>() {
+            @SuppressLint("MissingPermission")
             @Override
             public void subscribe(ObservableEmitter<T> emitter) {
                 if (!NetworkUtil.isConnectingToInternet(getContext())) {
